89bf19d5465a88dbb94301b5b5ed071fdd696a2b
meligera <meligera@ya.ru>
2022-05-14 12:43:57 +0300

    Alpha 0.3 (moved to if __name__, added comments)

--- client.py
+++ client.py
@@ -7,18 +7,21 @@ Welcome to the ChatRoom!
 nickname = input("Choose Your Nickname:\n")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 12345))
+client.connect(('localhost', 1995))
 
+# Threading control option
 stop_thread = False
 
 
 def recieve():
     while True:
+        # Adding a global variable
         global stop_thread
         if stop_thread:
             break
         try:
             message = client.recv(1024).decode('utf-8')
+            # Authorization, first time mandatory
             if message == 'NICK':
                 client.send(nickname.encode('utf-8'))
             else:
@@ -34,15 +37,15 @@ def write():
         if stop_thread:
             break
 
-        message = f'{nickname}: {input("")}'
-        if message[len(nickname) + 2:].startswith('/tell'):
+        message = f'{nickname}: {input("")}'  # Sending messages in special format
+        if message[len(nickname) + 2:].startswith('/tell'):  # Separate nickname from actual message
             client.send(f'TELL {nickname} {message[len(nickname) + 2 + 6:]}'.encode('utf-8'))
         elif message[len(nickname) + 2:].startswith('/list'):
             client.send(f'LIST {message[len(nickname) + 2 + 6:]}'.encode('utf-8'))
         elif message[len(nickname) + 2:].startswith('/help'):
             client.send(f'HELP {message[len(nickname) + 2 + 6:]}'.encode('utf-8'))
         else:
-            client.send(message.encode('utf-8'))
+            client.send(message.encode('utf-8'))  # If no command listed sending a message
 
 
 recieve_thread = threading.Thread(target=recieve)
--- server.py
+++ server.py
@@ -1,45 +1,47 @@
 import threading
 import socket
 
-PORT = 12345
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', PORT))
-server.listen()
-clients = []
-nicknames = []
-
-
+# broadcast to all users in chat
 def broadcast(message):
     for client in clients:
         client.send(message)
 
 
+# send a private message
 def send(message, client):
     client.send(message.encode('utf-8'))
 
 
+# function that will work with client`s requests or broadcast a message
 def handle(client):
     while True:
         try:
+            # Two variables are used to make it easier for threading to divide them
             msg = message = client.recv(1024)
-            if msg.decode('utf-8').startswith('TELL'):
+            # Checking if a message was a command
+            if msg.decode('utf-8').startswith('TELL'):  # Receive a private message
                 name_to_whisper = msg.decode('utf-8')
                 words = name_to_whisper.split(' ', 3)
-                print(words)
+                # There are strings that user doesn't see, so we split
+                # them [0] is a command, [1] nickname of sender,
+                # [2] is a receiver client, [3] is the rest of the message
+                # we don't split it
+                print(words)  # Debugging purposes
                 name = words[2]
                 mes = words[3]
-                full_mes = words[1] + " whispers: " + mes
+                full_mes = words[1] + " whispers: " + mes  # Creating a full structured message to send
                 whisper(full_mes, name)
-            elif msg.decode('utf-8').startswith('LIST'):
+            elif msg.decode('utf-8').startswith('LIST'):  # Message with a list of nicknames
                 client.send(f'Users in chatroom: {nicknames}'.encode('utf-8'))
-            elif msg.decode('utf-8').startswith('HELP'):
+            elif msg.decode('utf-8').startswith('HELP'):  # Message with a list of available commands
                 client.send('''
+A list of available commands:
 /tell       Send a private message to somebody (/tell user message)
 /list       Display users in chat
                 '''.encode('utf-8'))
             else:
-                broadcast(message)
+                broadcast(message)  # If there are no commands broadcasting a message to all
 
         except:
             if client in clients:
@@ -52,32 +54,43 @@ def handle(client):
                 break
 
 
+#  Function that accept clients and create a separate Thread for each of them
 def recieve():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-        client.send('NICK'.encode('utf-8'))
+        client.send('NICK'.encode('utf-8'))  # Auth message
         nickname = client.recv(1024).decode('utf-8')
 
-        nicknames.append(nickname)
-        clients.append(client)
+        nicknames.append(nickname)  # Adding a nickname to nickname list
+        clients.append(client)  # Adding a tech info to client list
 
         print(f'Nickname of the client is {nickname}')
-        broadcast(f'{nickname} has joined the Chat'.encode('utf-8'))
-        client.send('Connected to the Server!'.encode('utf-8'))
+        broadcast(f'{nickname} has joined the Chat'.encode('utf-8'))  # Telling everyone about a new user
+        client.send('Connected to the Server!'.encode('utf-8'))  # Confirming message to client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
 
+#  Private message function
 def whisper(message, name):
-    if name in nicknames:
-        name_index = nicknames.index(name)
-        client_to_whisper = clients[name_index]
-        send(message, client_to_whisper)
-        print(message)
-    else:
-        pass
+    if name in nicknames:  # Searching a user in nicknames
+        name_index = nicknames.index(name)  # Making a link with nickname index
+        client_to_whisper = clients[name_index]  # Order of lists are identical, take client's info
+        send(message, client_to_whisper)  # Call up a send function
+        print(message)  # Debugging purpose
 
 
-print('Server is Listening ...')
-recieve()
+if __name__ == '__main__':
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 1995))
+    server.listen()
+    clients = []  # List for technical information about users
+    nicknames = []  # List for nicknames
+    try:
+        print('Server is listening...')
+        recieve()
+    except KeyboardInterrupt:
+        print('How rude of you\n')
+        server.close()
+        quit()
